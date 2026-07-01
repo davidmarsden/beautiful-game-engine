2a43c68e435263b30915d3e4d07ac77df38fe355
davidmarsden <dmarsd@gmail.com>
2026-07-01 11:46:47 +0100

    Add initial club names

---
+++ src/world/clubNames.js
@@ -0,0 +1,12 @@
+export const CLUB_NAMES = Object.freeze([
+  "Alderwick", "Ashford Vale", "Barton Rovers", "Beacon City", "Blackwater", "Bramley Town", "Bridgeford", "Brookmere", "Calder United", "Canning Athletic",
+  "Carrowgate", "Castleford", "Chester Vale", "Claremont", "Cobalt City", "Copperfield", "Davenport", "Eastmere", "Eldon Athletic", "Elmbridge",
+  "Fairbourne", "Farsley City", "Fenwick", "Forest Gate", "Gable End", "Grantham Vale", "Greenhaven", "Greyfriars", "Halewood", "Harbour City",
+  "Hartcliffe", "Hazelwick", "Highbury Park", "Hollowmere", "Ironbridge", "Kingsport", "Lakeside", "Langford", "Larkhall", "Linden City",
+  "Lockwood", "Lowfield", "Mapleton", "Marshgate", "Meadowbank", "Merton Albion", "Millbrook", "Monksfield", "Northbridge", "Oakham",
+  "Oldbury", "Orchard Vale", "Oxbridge Town", "Parkhurst", "Pendleton", "Pineham", "Port Victoria", "Queensbury", "Radford", "Redhill",
+  "Rivermouth", "Rosewick", "Roxley", "Rushden Vale", "Saltford", "Sandbourne", "Seabrook", "Silvermere", "Southall", "Stamford Hill",
+  "Stonebridge", "Sutton Vale", "Tavistock", "Thornbury", "Tilehurst", "Trentham", "Union Wharf", "Upton Park", "Vale Athletic", "Victoria Dock",
+  "Wakefield Town", "Waltham", "Waterford Gate", "Westerby", "Westhaven", "Whitechapel", "Whitestone", "Willowbank", "Winchester Vale", "Woldham",
+  "Woodford", "Wycliffe", "Yardley", "Yorkgate", "Zephyr City", "Abbey Rangers", "Balfour United", "Canal City", "Dormers Wells", "Featherstone"
+]);
