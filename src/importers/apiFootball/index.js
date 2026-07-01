835d19986f2ab51493487bb63aed3eb6e380305a
davidmarsden <dmarsd@gmail.com>
2026-07-01 20:00:46 +0100

    Remove importer index from engine repo

--- src/importers/apiFootball/index.js
+++
@@ -1,6 +0,0 @@
-export { ApiFootballClient } from "./client.js";
-export { API_FOOTBALL_CONFIG, getApiFootballKey } from "./config.js";
-export {
-  normaliseApiFootballPlayer,
-  normaliseApiFootballPlayers
-} from "./normalise.js";
