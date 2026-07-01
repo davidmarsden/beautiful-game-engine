20968f8222ff097637a7947aa6c0df0ebcd7fb51
davidmarsden <dmarsd@gmail.com>
2026-07-01 15:15:34 +0100

    Add API-Football importer exports

---
+++ src/importers/apiFootball/index.js
@@ -0,0 +1,6 @@
+export { ApiFootballClient } from "./client.js";
+export { API_FOOTBALL_CONFIG, getApiFootballKey } from "./config.js";
+export {
+  normaliseApiFootballPlayer,
+  normaliseApiFootballPlayers
+} from "./normalise.js";
