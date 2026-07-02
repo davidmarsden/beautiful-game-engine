d338ebbb2f7e98bc207cd79287516a559f82241e
davidmarsden <dmarsd@gmail.com>
2026-07-02 22:40:01 +0100

    Add season module exports

---
+++ src/season/index.js
@@ -0,0 +1,11 @@
+export {
+  applyResultToTable,
+  createLeagueTable,
+  tableRows
+} from "./leagueTable.js";
+
+export {
+  createSeasonState,
+  processNextFixture,
+  seasonSummary
+} from "./seasonState.js";
