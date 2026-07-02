30b10fa3cef9daf19624a14c398f3e88c59b2925
davidmarsden <dmarsd@gmail.com>
2026-07-02 18:13:14 +0100

    Export real match helpers

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ export {
 } from "./lineups/index.js";
 export { resolveManagerPlan } from "./managers/index.js";
 export { expectedGoals, simulateFixture } from "./match/index.js";
+export { findFixtureByTeams } from "./realMatch/index.js";
 export { averageClubRating } from "./ratings/index.js";
 export { generateWorld } from "./world/generateWorld.js";
 export { createSeasonShell } from "./world/seasonShell.js";
