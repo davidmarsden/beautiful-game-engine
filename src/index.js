957de35b3df9ed85cb78fddbd19aba8999a58a34
davidmarsden <dmarsd@gmail.com>
2026-07-02 08:05:06 +0100

    Export fixture simulation API

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ export {
   summariseLeaguePack,
   validateLeaguePack
 } from "./leaguePack/index.js";
+export { expectedGoals, simulateFixture } from "./match/index.js";
 export { averageClubRating } from "./ratings/index.js";
 export { generateWorld } from "./world/generateWorld.js";
 export { createSeasonShell } from "./world/seasonShell.js";
