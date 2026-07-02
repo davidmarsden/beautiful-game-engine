9cf4b637d7b91cd1b495682d13adbf95739b328c
davidmarsden <dmarsd@gmail.com>
2026-07-02 21:57:33 +0100

    Add player condition exports

---
+++ src/playerCondition/index.js
@@ -0,0 +1,14 @@
+export {
+  effectiveRatingWithFatigue,
+  fatigueFromMinutes,
+  fatigueRatingAdjustment,
+  recoverFatigue,
+  recoveryRateForAge,
+  updateFatigueAfterMatch
+} from "./fatigue.js";
+
+export {
+  availabilityReason,
+  availabilityStatus,
+  isPlayerAvailable
+} from "./availability.js";
