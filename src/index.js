cc774b57e4dbe23e73384420055e8e43188ea438
davidmarsden <dmarsd@gmail.com>
2026-07-02 14:46:30 +0100

    Export lineup API

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ export {
   summariseLeaguePack,
   validateLeaguePack
 } from "./leaguePack/index.js";
+export {
+  FORMATIONS,
+  getFormationSlots,
+  playerRating,
+  playerRoles,
+  roleFitScore,
+  selectLineup
+} from "./lineups/index.js";
 export { expectedGoals, simulateFixture } from "./match/index.js";
 export { averageClubRating } from "./ratings/index.js";
 export { generateWorld } from "./world/generateWorld.js";
