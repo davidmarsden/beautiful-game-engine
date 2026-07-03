efe65e1d4b677807259b9537748f85b59caf8382
davidmarsden <dmarsd@gmail.com>
2026-07-03 10:22:42 +0100

    Export Monte Carlo calibration score

--- src/calibration/index.js
+++ src/calibration/index.js
@@ -4,6 +4,11 @@ export {
 } from "./leagueTableCalibration.js";
 
 export {
+  calibrateMonteCarlo,
+  formatMonteCarloCalibration
+} from "./monteCarloCalibration.js";
+
+export {
   calibrateSeasonDistribution,
   formatSeasonDistributionReport
 } from "./seasonDistributionCalibration.js";
